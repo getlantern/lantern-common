53af1210ca521b4f07bc6779cad18907c4f90e11
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-09-28 12:24:39 +0200

    #1017 Marked Client as allowing ignoring of unknown properties

--- src/main/java/org/lantern/Stats.java
+++ src/main/java/org/lantern/Stats.java
@@ -1,7 +1,8 @@
 package org.lantern;
 
+import com.google.appengine.repackaged.org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public interface Stats {
 
     long getUptime();
