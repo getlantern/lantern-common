a7f3a631d597bc0d30de6b01a3d77531ac3b5924
_pants <_pants@getlantern.org>
2013-11-20 15:30:47 -0500

    remove UPDATE_URL_KEY and UPDATE_RELEASE_KEY
    controller can nest this info in a mapping that UPDATE_KEY maps to

--- src/main/java/org/lantern/LanternConstants.java
+++ src/main/java/org/lantern/LanternConstants.java
@@ -67,14 +67,6 @@ public class LanternConstants {
      */
     public static final String UPDATE_KEY = "update";
 
-    /**
-     * maps to version's info url
-     * (e.g. "http://get-lantern.tumblr.com/post/xyz/lantern-1-0-0-beta7-just-went-live")
-     */
-    public static final String UPDATE_URL_KEY = "url";
-
-    public static final String UPDATE_RELEASE_DATE_KEY = "rd";
-
     public static final String INVITES_KEY = "invites";
 
     public static final String INVITED_EMAIL = "invem";
