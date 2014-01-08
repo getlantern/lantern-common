7471dbf50e22bb56389a11afccbb10b8f4244a3f
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-01-07 22:00:31 -0600

    #1282 Tweaked FriendResponse

--- src/main/java/org/lantern/messages/FriendResponse.java
+++ src/main/java/org/lantern/messages/FriendResponse.java
@@ -1,6 +1,8 @@
 package org.lantern.messages;
 
-import com.google.appengine.repackaged.org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * A response from the FriendEndpoint API.
@@ -8,11 +10,14 @@ import com.google.appengine.repackaged.org.codehaus.jackson.JsonNode;
  * @param <P>
  *            type of payload carried in this response
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FriendResponse<P> {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
     private boolean success;
     private int remainingFriendingQuota;
     private P payload;
-    private JsonNode payloadJson;
+    private String payloadJson;
 
     public FriendResponse() {
     }
@@ -24,6 +29,11 @@ public class FriendResponse<P> {
         this.success = true;
         this.remainingFriendingQuota = remainingFriendingQuota;
         this.payload = payload;
+        try {
+            this.payloadJson = MAPPER.writeValueAsString(payload);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean isSuccess() {
@@ -41,16 +51,26 @@ public class FriendResponse<P> {
     public void setRemainingFriendingQuota(int remainingFriendingQuota) {
         this.remainingFriendingQuota = remainingFriendingQuota;
     }
+    
+    public String getPayloadJson() {
+        return payloadJson;
+    }
 
+    public void setPayloadJson(String payloadJson) {
+        this.payloadJson = payloadJson;
+    }
+
+    @JsonIgnore
     public P getPayload() {
         return payload;
     }
 
-    public void setPayload(JsonNode payloadJson) {
-        this.payloadJson = payloadJson;
+    public <P2> P2 getPayloadAs(Class<P2> type) {
+        try {
+            return MAPPER.readValue(payloadJson, type);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public JsonNode getPayloadJson() {
-        return payloadJson;
-    }
 }
