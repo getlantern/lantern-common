071674c3c42a759616bedb5e33872cf66e4e4fd5
_pants <_pants@getlantern.org>
2014-01-10 15:03:17 -0500

    rewrite without reflection, allow google apps for domains

--- src/main/java/org/lantern/EmailAddressUtils.java
+++ src/main/java/org/lantern/EmailAddressUtils.java
@@ -1,43 +1,10 @@
 package org.lantern;
 
-import java.lang.reflect.Method;
 import java.util.Locale;
 
 import org.apache.commons.lang.StringUtils;
 
 public class EmailAddressUtils {
-
-    private static final String AT_DOMAIN = "@gmail.com";
-
-    @SuppressWarnings("unused")
-    private static String lowerCased(String input) {
-        return StringUtils.lowerCase(input, Locale.ENGLISH);
-    }
-
-    @SuppressWarnings("unused")
-    private static String strippedDots(String input) {
-        return StringUtils.replace(input, ".", "");
-    }
-
-    @SuppressWarnings("unused")
-    private static String strippedPlusExtension(String input) {
-        return StringUtils.substringBefore(input, "+");
-    }
-
-    private static Method[] normalizedEmailTransformations;
-    static {
-        try {
-            normalizedEmailTransformations = new Method[] {
-                    EmailAddressUtils.class.getDeclaredMethod("lowerCased", String.class),
-                    EmailAddressUtils.class.getDeclaredMethod("strippedDots", String.class),
-                    EmailAddressUtils.class.getDeclaredMethod("strippedPlusExtension", String.class),
-            };
-        }
-        catch (NoSuchMethodException e) {
-            System.err.println("NoSuchMethodException");
-        }
-    };
-
     /**
      * From https://github.com/getlantern/lantern/issues/1300:
      *
@@ -46,6 +13,9 @@ public class EmailAddressUtils {
      * case, '.'s and '+'s stripped from the username, and '@googlemail.com' replaced
      * with '@gmail.com'."
      *
+     * Domains other than gmail.com and googlemail.com are assumed to be Google Apps for
+     * Domains domains and are not changed.
+     *
      * If a string that is not of the expected form is passed in, no guarantee is
      * made about the result.
      *
@@ -53,11 +23,13 @@ public class EmailAddressUtils {
      * @return The normalized email address.
      */
     public static String normalizedEmail(String input) throws Exception {
-        String beforeAt = input.substring(0, input.indexOf("@"));
-        String transformed = beforeAt;
-        for (Method m : normalizedEmailTransformations) {
-            transformed = (String) m.invoke(EmailAddressUtils.class, transformed);
-        }
-        return transformed + AT_DOMAIN;
+        input = input.toLowerCase(Locale.ENGLISH);
+        int idx = input.indexOf("@");
+        String beforeAt = input.substring(0, idx);
+        String rest = input.substring(idx);
+        beforeAt = beforeAt.replaceAll("\\.", "");
+        beforeAt = StringUtils.substringBefore(beforeAt, "+");
+        rest = rest.replaceAll("googlemail.com$", "gmail.com");
+        return beforeAt + rest;
     }
 }
