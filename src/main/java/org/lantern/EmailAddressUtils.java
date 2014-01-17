3817d9b9420cfff19067a8f5f3fa71e11e8885aa
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-01-16 22:58:04 -0600

    Gradlefied controller build

--- src/main/java/org/lantern/EmailAddressUtils.java
+++ src/main/java/org/lantern/EmailAddressUtils.java
@@ -2,7 +2,7 @@ package org.lantern;
 
 import java.util.Locale;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class EmailAddressUtils {
     /**
