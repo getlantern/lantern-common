1a00a371e827631b9a006b7a8c2d20d90932f4c7
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-13 17:03:07 -0500

    getlantern/lantern#1427 Moved StatshubAPI into common

--- src/main/java/org/lantern/monitoring/Stats.java
+++ src/main/java/org/lantern/monitoring/Stats.java
@@ -7,6 +7,29 @@ import java.util.Map;
  * Generic representation of statistics.
  */
 public class Stats {
+    public static enum CounterKey {
+        requestsGiven,
+        bytesGiven,
+        requestsGivenByFallback,
+        bytesGivenByFallback,
+        requestsGivenByPeer,
+        bytesGivenByPeer,
+        requestsGotten,
+        bytesGotten,
+        directBytes,
+    };
+
+    public static enum GaugeKey {
+        usingUPnP,
+        usingNATPMP,
+        bpsGiven,
+        bpsGivenByFallback,
+        bpsGivenByPeer,
+        bpsGotten,
+        distinctPeers,
+        online
+    };
+
     private volatile Map<String, Long> counter = new HashMap<String, Long>();
     private volatile Map<String, Long> increment = new HashMap<String, Long>();
     private volatile Map<String, Long> gauge = new HashMap<String, Long>();
@@ -21,11 +44,11 @@ public class Stats {
     public void setCounter(Map<String, Long> counter) {
         this.counter = counter;
     }
-    
+
     public Map<String, Long> getIncrement() {
         return increment;
     }
-    
+
     public void setIncrement(Map<String, Long> increment) {
         this.increment = increment;
     }
@@ -38,16 +61,24 @@ public class Stats {
         this.gauge = gauge;
     }
 
-    public void setCounter(String name, long value) {
-        counter.put(name, value);
-    }
-    
-    public void setIncrement(String name, long value) {
-        counter.put(name, value);
+    public void setCounter(CounterKey name, long value) {
+        counter.put(name.toString(), value);
     }
 
-    public void setGauge(String name, long value) {
-        gauge.put(name, value);
+    public void setCounter(CounterKey name, String country, long value) {
+        counter.put(name.toString() + "_" + country, value);
+    }
+
+    public void setIncrement(CounterKey name, long value) {
+        increment.put(name.toString(), value);
+    }
+
+    public void setIncrement(CounterKey name, String country, long value) {
+        increment.put(name.toString() + "_" + country, value);
+    }
+
+    public void setGauge(GaugeKey name, long value) {
+        gauge.put(name.toString(), value);
     }
 
 }
