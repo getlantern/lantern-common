3192a2bfd1e63ba3c1407630eb68a3ccdc6c28f2
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-13 16:51:47 -0500

    getlantern/lantern#1427 Moved StatshubAPI into common

---
+++ src/main/java/org/lantern/monitoring/Stats.java
@@ -0,0 +1,53 @@
+package org.lantern.monitoring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Generic representation of statistics.
+ */
+public class Stats {
+    private volatile Map<String, Long> counter = new HashMap<String, Long>();
+    private volatile Map<String, Long> increment = new HashMap<String, Long>();
+    private volatile Map<String, Long> gauge = new HashMap<String, Long>();
+
+    public Stats() {
+    }
+
+    public Map<String, Long> getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Map<String, Long> counter) {
+        this.counter = counter;
+    }
+    
+    public Map<String, Long> getIncrement() {
+        return increment;
+    }
+    
+    public void setIncrement(Map<String, Long> increment) {
+        this.increment = increment;
+    }
+
+    public Map<String, Long> getGauge() {
+        return gauge;
+    }
+
+    public void setGauge(Map<String, Long> gauge) {
+        this.gauge = gauge;
+    }
+
+    public void setCounter(String name, long value) {
+        counter.put(name, value);
+    }
+    
+    public void setIncrement(String name, long value) {
+        counter.put(name, value);
+    }
+
+    public void setGauge(String name, long value) {
+        gauge.put(name, value);
+    }
+
+}
---
+++ src/main/java/org/lantern/monitoring/StatsHubAPI.java
@@ -0,0 +1,94 @@
+package org.lantern.monitoring;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lantern.HttpURLClient;
+import org.lantern.JsonUtils;
+import org.lantern.LanternConstants;
+
+/**
+ * Base of API for submitting stats to StatsHub.
+ */
+public class StatsHubAPI extends HttpURLClient {
+    public StatsHubAPI() {
+        this(null);
+    }
+
+    public StatsHubAPI(InetSocketAddress proxyAddress) {
+        super(proxyAddress);
+    }
+
+    /**
+     * Submits stats to statshub.
+     * 
+     * @param id
+     *            the id (instance or user)
+     * @param countryCode
+     *            (the country code, xx for unknown)
+     * @param stats
+     *            the stats
+     */
+    public void postStats(String id, String countryCode, Stats stats)
+            throws Exception {
+        Map<String, Object> request = new HashMap<String, Object>();
+        request.put("countryCode", countryCode);
+        request.put("counter", stats.getCounter());
+        request.put("gauge", stats.getGauge());
+
+        HttpURLConnection conn = newConn(urlFor(id));
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        OutputStream out = conn.getOutputStream();
+        try {
+            JsonUtils.OBJECT_MAPPER.writeValue(out, request);
+            int code = conn.getResponseCode();
+            if (code != 200) {
+                // will be logged below
+                throw new Exception("Got " + code + " response:\n"
+                        + conn.getResponseMessage());
+            }
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ioe) {
+                // ignore
+            }
+        }
+    }
+
+    public StatsResponse getStats(String id) throws Exception {
+        HttpURLConnection conn = newConn(urlFor(id));
+        conn.setRequestMethod("GET");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        InputStream in = conn.getInputStream();
+        try {
+            int code = conn.getResponseCode();
+            if (code != 200) {
+                // will be logged below
+                throw new Exception("Got " + code + " response:\n"
+                        + conn.getResponseMessage());
+            }
+            return JsonUtils.decode(in, StatsResponse.class);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException ioe) {
+                // ignore
+            }
+        }
+    }
+
+    private String urlFor(String instanceId) {
+        return LanternConstants.statshubBaseAddress
+                + instanceId;
+    }
+
+}
