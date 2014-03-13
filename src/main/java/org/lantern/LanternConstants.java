bfd275f3c0ec18387b81197c5464df49546c2ae7
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-13 07:16:30 -0500

    getlantern/lantern#1427 Starting to update lantern-common for new stats infrastructure

--- src/main/java/org/lantern/LanternConstants.java
+++ src/main/java/org/lantern/LanternConstants.java
@@ -40,6 +40,8 @@ public class LanternConstants {
     public static final InetSocketAddress LANTERN_LOCALHOST_ADDR =
             new InetSocketAddress("127.0.0.1",
                     LANTERN_LOCALHOST_HTTP_PORT);
+    
+    public static final String statshubBaseAddress = "https://pure-journey-3547.herokuapp.com/stats/";
 
     public static final String USER_NAME = "un";
     public static final String PASSWORD = "pwd";
